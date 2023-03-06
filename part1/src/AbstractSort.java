e89d4f7eef60a0ceb7abe1a266283163f5571214
kangchanghan <anycar8515@ynu.ac.kr>
2023-03-07 03:34:04 +0900

    Feat : AbstractSort 및 Comparable 예제(DateCustom) 구현

---
+++ part1/src/AbstractSort.java
@@ -0,0 +1,33 @@
+package src;
+
+public abstract class AbstractSort {
+    public static void sort(Comparable[] list) {
+
+    }
+
+    protected static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    protected static void exchange(Comparable[] list, int i, int j) {
+        Comparable t = list[i];
+        list[i] = list[j];
+        list[j] = t;
+    }
+
+    protected static void compareTo(Comparable o1) {
+
+    }
+
+    protected static void show(Comparable[] list) {
+        for (Comparable a : list) // == for (int i=0;i<list.length;i++)
+            System.out.print(a + " "); // == System.out.print(list[i] + " ")
+        System.out.println();
+    }
+
+    protected static boolean isSorted(Comparable[] a) {
+        for (int i=1;i<a.length;i++)
+            if (less(a[i], a[i-1])) return false;
+        return true;
+    }
+}
---
+++ part1/src/ComparableMain.java
@@ -0,0 +1,68 @@
+package src;
+
+public class ComparableMain {
+
+    public static class DateCustom implements Comparable<DateCustom> {
+        private final int year;
+        private final int month;
+        private final int day;
+
+        public DateCustom(int year, int month, int day) {
+            if(!isValid(year,month, day)) throw new IllegalArgumentException("Invalid date");
+            this.year = year;
+            this.month = month;
+            this.day = day;
+        }
+
+        @Override
+        public int compareTo(DateCustom o1) {
+            if (this.year < o1.year) return -1;
+            if (this.year > o1.year) return +1;
+            if (this.month < o1.month) return -1;
+            if (this.month > o1.month) return +1;
+            if (this.day < o1.day) return -1;
+            if (this.day > o1.day) return +1;
+            return 0;
+        }
+
+        public static boolean isValid(int year, int month, int day) {
+            Integer date = null;
+
+            if(month < 1 || month > 12) return false;
+
+            //윤년 체크
+            if(month == 2) {
+                if(isLeapYear(year))
+                    date = 29;
+                else
+                    date = 28;
+            }
+            //일수 체크
+            date = switch (month) {
+                case 1, 3, 5, 7, 8, 10, 12 -> 31;
+                case 4, 6, 9, 11 -> 30;
+                default -> 0;
+            };
+            ;
+
+            if (date == null || day != date)
+                return false;
+
+            return true;
+        }
+
+        public static boolean isLeapYear(int year) {
+            if(year % 4 == 0) {
+                if (year % 400 == 0) return true;
+                else if (year % 100 == 0) return false;
+                return true;
+            }
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
